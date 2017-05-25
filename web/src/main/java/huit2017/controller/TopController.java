f3bf54bc1fa6092e6dc2600dd24d9dbd6829d387
yamakawa <yamakawa@leon-tec.co.jp>
2017-05-25 15:36:29 +0900

    TOPコントローラー jsp新規作成

--- web/src/main/java/huit2017/controller/TopController.java
+++ web/src/main/java/huit2017/controller/TopController.java
@@ -1,19 +1,23 @@
 package huit2017.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import huit2017.entity.Bbs;
+import huit2017.mapper.BbsMapper;
+
 @Controller
 public class TopController {
 
-//    @Autowired
-//    private BbsMapper bbsMapper;
+    @Autowired
+    private BbsMapper bbsMapper;
 
     @RequestMapping(value = "/index", method = RequestMethod.GET)
     public String index() {
-     //   Bbs bbs = bbsMapper.selectByPrimaryKey(1);
-      //  System.out.println("comment: " + bbs.getComment());
+        Bbs bbs = bbsMapper.selectByPrimaryKey(1);
+        System.out.println("comment: " + bbs.getComment());
         return "index";
     }
 }
---
+++ web/src/main/webapp/WEB-INF/view/index.jsp
@@ -0,0 +1,9 @@
+<!DOCTYPE>
+<html lang="ja">
+<head>
+<title>トップ</title>
+</head>
+<body>
+	<h2>日本語</h2>
+</body>
+</html>
