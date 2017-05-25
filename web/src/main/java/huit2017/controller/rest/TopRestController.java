f45fa64b1488b590023bb326b339d65d95422879
yamakawa <hachidaiyamakawa@gmail.com>
2017-05-25 22:42:19 +0900

    JSON修正

--- web/src/main/java/huit2017/controller/rest/TopRestController.java
+++ web/src/main/java/huit2017/controller/rest/TopRestController.java
@@ -1,7 +1,8 @@
 package huit2017.controller.rest;
 
 import java.util.Arrays;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class TopRestController {
 
     @RequestMapping(path = "/list")
-    public List<String> list() {
-       return Arrays.asList("紀伊", "太郎", "紀伊", "花子");
+    public Map<String, Object> list() {
+
+       Map<String, Object> response = new HashMap<>();
+       response.put("a", Arrays.asList("紀伊", "太郎", "紀伊", "花子"));
+       response.put("b", Arrays.asList("紀伊", "太郎", "紀伊", "花子"));
+       return response;
     }
 }
