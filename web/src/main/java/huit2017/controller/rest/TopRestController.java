5a9508ad102df59e8eaa6708b98000b9c8c9e252
yamakawa <hachidaiyamakawa@gmail.com>
2017-05-27 18:05:31 +0900

    JSON リクエスト対応

--- web/src/main/java/huit2017/controller/rest/TopRestController.java
+++ web/src/main/java/huit2017/controller/rest/TopRestController.java
@@ -4,14 +4,17 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import huit2017.request.BbsRequest;
 
 @RestController
 public class TopRestController {
 
     @RequestMapping(path = "/list")
-    public Map<String, Object> list() {
+    public Map<String, Object> list(@RequestBody BbsRequest request) {
 
        Map<String, Object> response = new HashMap<>();
        response.put("a", Arrays.asList("紀伊", "太郎", "紀伊", "花子"));
---
+++ web/src/main/java/huit2017/request/BbsRequest.java
@@ -0,0 +1,9 @@
+package huit2017.request;
+
+import lombok.Data;
+
+@Data
+public class BbsRequest {
+
+    private Integer bbsId;
+}
