12b0c8f24b63d4e60b497829d54a3aad9bce5a01
山川 八大 <hachidaiyamakawa@gmail.com>
2017-11-09 00:45:52 +0900

    Add Junit

---
+++ web/src/test/java/huit2017/service/impl/TopServiceImplTest.java
@@ -0,0 +1,14 @@
+package huit2017.service.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TopServiceImplTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
